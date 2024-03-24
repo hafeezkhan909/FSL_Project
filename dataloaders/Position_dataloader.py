8baa32878194f0a6f2bd9e6c971089045a323fa4
Mohammed Abdul Hafeez Khan <f20190091@dubai.bits-pilani.ac.in>
2024-03-24 17:25:14 -0400

    First successful training iteration.

--- dataloaders/Position_dataloader.py
+++ dataloaders/Position_dataloader.py
@@ -2,90 +2,50 @@ import torch
 from torch.utils.data import Dataset
 from data_process.data_process_func import *
 from multiprocessing import Pool, cpu_count
+import random
 
-def create_sample1(image_path, out_size):
-    """
-    Create a sample dictionary with image data loaded from a JPEG file.
+def create_sample1(image_path):
 
-    Args:
-        image_path (str): Path to the JPEG image.
-        out_size (tuple): Desired output size as (height, width).
-
-    Returns:
-        dict: A dictionary containing the image tensor and additional information.
-    """
     image = load_png_image_as_array1(image_path)
     image = image.astype(np.float32)
 
-    if out_size:
-        if image.shape[0] <= out_size[0] or image.shape[1] <= out_size[1]:
-            ph = max((out_size[0] - image.shape[0]) // 2 + 3, 0)
-            pw = max((out_size[1] - image.shape[1]) // 2 + 3, 0)
-            image = np.pad(image, [(ph, ph), (pw, pw), (0, 0)], mode='constant', constant_values=0)
-
     # Ensure the image has a batch dimension for PyTorch compatibility
-    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    image = np.expand_dims(image, axis=0)  # Add channel dimension
     sample = {'image': torch.from_numpy(image), 'image_path': image_path}
+    # print(sample)
     return sample
 
 class PositionDataloader1(Dataset):
-    """ Dataset position """
+    """Dataset position"""
 
-    def __init__(self, config=None, image_name_list='train', num=None, transform=None,
-                 random_sample=True, load_aug=False, load_memory=True, out_size=None):
+    def __init__(self, config=None, image_path='../data/Reconstructed_Static_64_Frame.png', transform=None,
+                 random_sample=True, out_size=None):
         self._iternum = config['iter_num']
         self.out_size = out_size
         self.transform = transform
-        self.sample_list = []
-        self.image_dic = {}
-        image_task_dic = {}
-        self.iternum = 0
-        self.load_aug = load_aug
-        self.load_memory = load_memory
+        self.image_path = image_path  # Directly use the image path
         self.random_sample = random_sample
-        self.image_name_list = read_file_list(image_name_list)
 
-        if load_memory:
-            # p = Pool(2)
-            p = Pool(cpu_count())
-            for image_name in self.image_name_list:
-                image_task_dic[image_name] = p.apply_async(create_sample1, args=(image_name, out_size,))
-            p.close()
-            p.join()
-            for image_name in image_task_dic.keys():
-                self.image_dic[image_name] = image_task_dic[image_name].get()
-
-        if num is not None:
-            self.image_name_list = self.image_name_list[:num]
-        print("total {} samples".format(len(self.image_name_list)))
+        # print("Total 1 sample")  # Only one sample since it's a single image
 
     def __len__(self):
-        if self.random_sample:
-            return self._iternum
-        else:
-            return len(self.image_name_list)
+        # Return the number of iterations per epoch
+        return self._iternum
 
     def __getitem__(self, idx):
-        if self.load_memory:
-            sample = self.image_dic[random.sample(self.image_name_list, 1)[0]].copy()
-        else:
-            if self.random_sample:
-                image_name = random.sample(self.image_name_list, 1)[0]
-            else:
-                image_name = self.image_name_list[idx]
-            sample = create_sample1(image_name, self.out_size)
+        # Load image on demand
+        sample = create_sample1(self.image_path)
+        # print("sample after create_sample1", sample)
+
+        # Apply transformations if specified
         if self.transform:
             sample = self.transform(sample)
+            # print("sample after transformation", sample)
+
         return sample
 
+
 class RandomDoubleCrop1(object):
-    """
-    Randomly crop several images in one sample;
-    distance is a vector (could be positive or negative), representing the vector
-    from image1 to image2.
-    Args:
-    output_size (tuple): Desired output size (height, width)
-    """
 
     def __init__(self, output_size, foreground_only=True, small_move=False, fluct_range=[0, 0]):
         self.output_size = torch.tensor(output_size, dtype=torch.int16)
@@ -94,21 +54,23 @@ class RandomDoubleCrop1(object):
         self.fluct_range = fluct_range  # distance, could be in pixels
         self.small_move = small_move
         # Only two dimensions are needed for 2D images
-        cor_x, cor_y = np.meshgrid(np.arange(self.output_size[1]),
-                                   np.arange(self.output_size[0]))
-        self.cor_grid = torch.from_numpy(np.concatenate((cor_x[np.newaxis],
-                                                         cor_y[np.newaxis]), axis=0))
+        # cor_x, cor_y = np.meshgrid(np.arange(self.output_size[1]),
+        #                            np.arange(self.output_size[0]))
+        # self.cor_grid = torch.from_numpy(np.concatenate((cor_x[np.newaxis],
+        #                                                  cor_y[np.newaxis]), axis=0))
 
     def random_position(self, shape, initial_position=[0, 0], small_move=False):
         position = []
-        for i in range(len(shape)):  # This will now iterate twice (for x and y)
+        # Start from index 1 to skip the channel dimension
+        for i in range(1, len(shape)):  # This will iterate over height and width only
             if small_move:
                 # Calculate the position with consideration for small_move and fluct_range
-                pos_min = max(0, initial_position[i] - self.fluct_range[i])
-                pos_max = min(shape[i] - 1, initial_position[i] + self.fluct_range[i])
+                pos_min = max(0, initial_position[i - 1] - self.fluct_range[i - 1])
+                pos_max = min(shape[i] - 1, initial_position[i - 1] + self.fluct_range[i - 1])
                 position.append(random.randint(pos_min, pos_max))
             else:
                 # Random position without small_move considerations
+                # print(shape[i])
                 position.append(random.randint(0, shape[i] - 1))
         return torch.tensor(position, dtype=torch.int16)
 
@@ -119,25 +81,33 @@ class RandomDoubleCrop1(object):
 
         background_chosen = True
         shape_n = image.shape  # Directly using numpy shape property.
+        # print("This is the shape being used", shape_n)
         while background_chosen:
             random_pos0 = self.random_position(shape_n)
             # Check if the randomly selected position is foreground if foreground_only is True.
-            if not self.foreground_only or image[random_pos0[1], random_pos0[0]] >= 45:
+            if not self.foreground_only or image[0, random_pos0[0], random_pos0[1]] >= 45:
                 background_chosen = False
 
-        half_size = (self.output_size // 2).numpy()  # Assuming output_size is a tensor.
-        top_left = np.maximum(random_pos0 - half_size, 0)
-        bottom_right = np.minimum(random_pos0 + half_size, shape_n)
+        half_size = np.array(self.output_size) // 2  # Assuming output_size is a tensor.
+        top_left = np.maximum(random_pos0 - half_size, [0, 0])
+        bottom_right = np.minimum(random_pos0 + half_size, shape_n[1:])
 
         # Crop the image
-        cropped_image = image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+        cropped_image = image[:, top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+        # Determine the amount of padding needed
+        padding = [(0, 0)]  # No padding for channels
+        for i in range(2):
+            low_pad = half_size[i] - (random_pos0[i] - top_left[i])
+            high_pad = half_size[i] - (bottom_right[i] - random_pos0[i])
+            padding.append((low_pad, high_pad))
 
-        # Calculate padding
-        pad_width = ((abs(min(0, top_left[1])), max(0, bottom_right[1] - shape_n[0])),
-                     (abs(min(0, top_left[0])), max(0, bottom_right[0] - shape_n[1])))
+        # Apply padding
+        padded_image = np.pad(cropped_image, padding, mode='constant', constant_values=0)
 
-        # Pad the image
-        padded_image = np.pad(cropped_image, pad_width, mode='constant', constant_values=0)
+        # Ensure the output image has the desired size
+        padded_image = padded_image[:, :self.output_size[0], :self.output_size[1]]
+
+        # print("Cropped image 0 shape: ", padded_image.shape)
         nsample['random_crop_image_0'] = padded_image
 
         nsample['random_position_0'] = random_pos0
@@ -148,8 +118,8 @@ class RandomDoubleCrop1(object):
         #     nsample['visualized_image'] = vis_image  # Store or save this image as needed
 
         # Ensure random_pos0 is a tensor for this operation
-        random_pos0_tensor = torch.tensor(random_pos0, dtype=torch.float32).view(2, 1, 1)
-        nsample['random_fullsize_position_0'] = (self.cor_grid + random_pos0_tensor).numpy()
+        # random_pos0_tensor = torch.tensor(random_pos0, dtype=torch.float32).view(2, 1, 1)
+        # nsample['random_fullsize_position_0'] = (self.cor_grid + random_pos0_tensor).numpy()
 
         # 2nd Crop
 
@@ -157,21 +127,27 @@ class RandomDoubleCrop1(object):
         while background_chosen:
             random_pos1 = self.random_position(shape_n, nsample['random_position_0'], self.small_move)
             # Check the condition for the selected pixel; if it's foreground, break the loop.
-            if not self.foregroung_only or image[random_pos1[0], random_pos1[1]] >= 45:
+            if not self.foreground_only or image[0, random_pos1[0], random_pos1[1]] >= 45:
                 background_chosen = False
 
-        top_left1 = np.maximum(random_pos1 - half_size, 0)
-        bottom_right1 = np.minimum(random_pos1 + half_size, shape_n)
+        top_left1 = np.maximum(random_pos1 - half_size, [0, 0])
+        bottom_right1 = np.minimum(random_pos1 + half_size, shape_n[1:])
 
         # Crop the image
-        cropped_image1 = image[top_left1[1]:bottom_right1[1], top_left1[0]:bottom_right1[0]]
+        cropped_image1 = image[:, top_left1[0]:bottom_right1[0], top_left1[1]:bottom_right1[1]]
+        # Determine the amount of padding needed
+        padding1 = [(0, 0)]  # No padding for channels
+        for i in range(2):
+            low_pad1 = half_size[i] - (random_pos1[i] - top_left1[i])
+            high_pad1 = half_size[i] - (bottom_right1[i] - random_pos1[i])
+            padding1.append((low_pad1, high_pad1))
 
-        # Calculate padding
-        pad_width1 = ((abs(min(0, top_left1[1])), max(0, bottom_right1[1] - shape_n[0])),
-                     (abs(min(0, top_left1[0])), max(0, bottom_right1[0] - shape_n[1])))
+        # Apply padding
+        padded_image1 = np.pad(cropped_image1, padding1, mode='constant', constant_values=0)
 
-        # Pad the image
-        padded_image1 = np.pad(cropped_image1, pad_width1, mode='constant', constant_values=0)
+        # Ensure the output image has the desired size
+        padded_image1 = padded_image1[:, :self.output_size[0], :self.output_size[1]]
+        # print("Cropped image 1 shape: ", padded_image1.shape)
         nsample['random_crop_image_1'] = padded_image1
 
         nsample['random_position_1'] = random_pos1
@@ -182,8 +158,8 @@ class RandomDoubleCrop1(object):
         #     nsample['visualized_image1'] = vis_image1  # Store or save this image as needed
 
         # Ensure random_pos1 is a tensor for this operation
-        random_pos1_tensor = torch.tensor(random_pos1, dtype=torch.float32).view(2, 1, 1)
-        nsample['random_fullsize_position_1'] = (self.cor_grid + random_pos1_tensor).numpy()
+        # random_pos1_tensor = torch.tensor(random_pos1, dtype=torch.float32).view(2, 1, 1)
+        # nsample['random_fullsize_position_1'] = (self.cor_grid + random_pos1_tensor).numpy()
 
         key_ls = list(nsample.keys())
         for key in key_ls:
@@ -192,39 +168,13 @@ class RandomDoubleCrop1(object):
 
         return nsample
 
-class RandomDoubleMask1(object):
-    def __init__(self, max_round=1, include_0=['random_crop_image_0'], include_1=['random_crop_image_1'],
-                 mask_size=[0, 0]):
-        self.include_0 = include_0
-        self.include_1 = include_1
-        self.max_round = max_round
-        self.mask_size = mask_size  # Should now be [height, width]
-
-    def __call__(self, sample):
-        for include in self.include_0[0], self.include_1[0]:
-            max_round = np.random.randint(1, self.max_round + 1)
-            sample[include.replace('random', 'random_mask')] = torch.clone(sample[include])
-            mask_image = sample[include].clone()
-            shape = mask_image.shape[1:]  # Shape is now just height and width
-
-            for _ in range(max_round):
-                min_cor = []
-                for i in range(2):  # Loop over 2 dimensions instead of 3
-                    min_cor.append(np.random.randint(0, shape[i] - self.mask_size[i]))
-
-                # Apply the mask
-                sample[include.replace('random', 'random_mask')][:,
-                min_cor[0]:min_cor[0] + self.mask_size[0], min_cor[1]:min_cor[1] + self.mask_size[1]] = 0
-
-        return sample
-
 class ToPositionTensor1(object):
     """Convert ndarrays in sample to Tensors for 2D images."""
 
     def __call__(self, sample):
 
         sample['rela_distance'] = sample['random_position_0'] - sample['random_position_1']
-        sample['rela_fullsize_distance'] = (sample['random_fullsize_position_0'] - sample['random_fullsize_position_1'])
+        # sample['rela_fullsize_distance'] = (sample['random_fullsize_position_0'] - sample['random_fullsize_position_1'])
 
         return sample
 
--- networks/PRNet.py
+++ networks/PRNet.py
@@ -7,7 +7,7 @@ import math
 import numpy as np
 
 class PRNet(t.nn.Module):
-    def __init__(self,  inc=1, patch_size=1, n_classes=5, base_chns=12, droprate=0, norm='in', dilation=1):
+    def __init__(self,  inc=1, patch_size=1, n_classes=1, base_chns=12, droprate=0, norm='in', dilation=1):
         super(PRNet, self).__init__()
         self.model_name = "seg"
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
@@ -39,65 +39,101 @@ class PRNet(t.nn.Module):
         self.conv7_2 = SingleConv2D(2 * base_chns,  base_chns, norm=norm, dilation=dilation, padding='same')
 
         self.conv8_1 = SingleConv2D(2*base_chns, base_chns, norm=norm, dilation=dilation, padding='same')
-        self.conv8_2 = SingleConv2D( base_chns, base_chns, norm=norm, dilation=dilation, padding='same')
+        self.conv8_2 = SingleConv2D(base_chns, base_chns, norm=norm, dilation=dilation, padding='same')
 
         self.classification = nn.Sequential(
             nn.Conv2d(in_channels=base_chns, out_channels=n_classes, kernel_size=1),
         )
         # fc_inc = int(np.asarray(patch_size).prod()/4096)*16*base_chns
-        fc_inc = int(np.asarray(patch_size).prod()) * 16 * base_chns
-        self.fc1 = nn.Linear(fc_inc, 8 * base_chns)
+        fc_inc = int(np.asarray(patch_size).prod())
+        self.fc1 = nn.Linear(2304, 8 * base_chns)
         self.fc2 = nn.Linear(8 * base_chns, 4 * base_chns)
         self.fc3 = nn.Linear(4 * base_chns, 2)
 
     def forward(self, x, out_feature=True):
         out = self.conv0_1(x)
+        # print('conv0_1 output: ', out.shape)
         conv0 = self.conv0_2(out)
+        # print('conv0_2 output: ', conv0.shape)
         out = self.downsample(conv0)
+        # print('conv0_2 downsampled output: ', out.shape)
         out = self.conv1_1(out)
+        # print('conv1_1 output: ', out.shape)
         conv1 = self.conv1_2(out)
+        # print('conv1_2 output: ', conv1.shape)
         out = self.downsample(conv1)  # 1/2
+        # print('conv1_2 downsampled output: ', out.shape)
         out = self.conv2_1(out)
+        # print('conv2_1 output: ', out.shape)
         conv2 = self.conv2_2(out)  #
+        # print('conv2_2 output: ', conv2.shape)
         out = self.downsample(conv2)  # 1/4
+        # print('conv2_2 downsampled output: ', out.shape)
         out = self.conv3_1(out)
+        # print('conv3_1 output: ', out.shape)
         conv3 = self.conv3_2(out)  #
+        # print('conv3_2 output: ', conv3.shape)
         out = self.downsample(conv3)  # 1/8
+        # print('conv3_2 downsampled output: ', out.shape)
         out = self.conv4_1(out)
+        # print('conv4_1 output: ', out.shape)
         out = self.conv4_2(out)
+        # print('conv4_2 output: ', out.shape)
         #out = self.drop(out)
 
         fc_out = out.view(out.shape[0],-1)
+        # print("fc_out output", fc_out.shape)
         fc_out = self.fc1(fc_out)
+        # print('fc1 output: ', fc_out.shape)
         fc_out = self.fc2(fc_out)
+        # print('fc2 output: ', fc_out.shape)
         fc_out = self.fc3(fc_out)
+        # print('fc3 output: ', fc_out.shape)
 
         up5 = self.upsample(out)  # 1/4
+        # print('up5 output: ', up5.shape)
         out = t.cat((up5, conv3), 1)
+        # print('concatenating up5 and conv3_2 output: ', out.shape)
         out = self.conv5_1(out)
+        # print('conv5_1 output: ', out.shape)
         conv5 = self.conv5_2(out)
+        # print('conv5_2 output: ', conv5.shape)
         center_feature5 = conv5[:, :, conv5.shape[2]//2, conv5.shape[3]//2]
 
         up6 = self.upsample(conv5)  # 1/2
+        # print('up6 output: ', up6.shape)
         out = t.cat((up6, conv2), 1)
+        # print('concatenating up6 and conv2_2 output: ', out.shape)
         out = self.conv6_1(out)
+        # print('conv6_1 output: ', out.shape)
         conv6 = self.conv6_2(out)
+        # print('conv6_2 output: ', conv6.shape)
         center_feature6 = conv6[:, :, conv6.shape[2]//2, conv6.shape[3]//2]
 
         up7 = self.upsample(out)
+        # print('up7 output: ', up7.shape)
         out = t.cat((up7, conv1), 1)
+        # print('concatenating up7 and conv1_2 output: ', out.shape)
         out = self.conv7_1(out)
+        # print('conv7_1 output: ', out.shape)
         conv7 = self.conv7_2(out)
+        # print('conv7_2 output: ', conv7.shape)
         center_feature7 = conv7[:, :, conv7.shape[2]//2, conv7.shape[3]//2]
 
         up8 = self.upsample(conv7)
+        # print('up8 output: ', up8.shape)
         out = t.cat((up8, conv0), 1)
+        # print('concatenating up8 and conv0_2 output: ', out.shape)
         out = self.conv8_1(out)
+        # print('conv8_1 output: ', out.shape)
         conv8 = self.conv8_2(out)
+        # print('conv8_2 output: ', conv8.shape)
         center_feature8 = conv8[:, :, conv8.shape[2]//2, conv8.shape[3]//2]
 
         out = self.classification(out)
+        # print('classification output: ', out.shape)
         dic = {'fc_position': fc_out, 'ae': out}
+        # print(dic)
         if out_feature:
             dic['center_feature5'] = center_feature5
             dic['center_feature6'] = center_feature6
