Fixed the bug! Floating point needed Normalization (/255).

dataloaders/Position_dataloader.py
@@ -9,7 +9,7 @@ def create_sample1(image_path):
 
     image = load_png_image_as_array1(image_path)
     image = image.astype(np.float32)
-
+    # image /= 255
     # Ensure the image has a batch dimension for PyTorch compatibility
     image = np.expand_dims(image, axis=0)  # Add channel dimension
     sample = {'image': torch.from_numpy(image), 'image_path': image_path}
@@ -73,7 +73,7 @@ class RandomDoubleCrop1(object):
                 # Random position without small_move considerations
                 # print(shape[i])
                 position.append(random.randint(0, shape[i] - 1))
-        print(position)
+        # print(position)
         return torch.tensor(position, dtype=torch.int16)
 
     def __call__(self, sample):
@@ -82,10 +82,10 @@ class RandomDoubleCrop1(object):
         nsample['image_path'] = sample['image_path']
         copied_image = image.numpy()
         copied_image = np.squeeze(copied_image)
-        print(copied_image.shape)
-        print(type(copied_image))
-        cv2.imshow("Image", copied_image)
-        cv2.waitKey(0)
+        # print(copied_image.shape)
+        # print(type(copied_image))
+        # cv2.imshow("Image", copied_image)
+        # cv2.waitKey(0)
         background_chosen = True
         shape_n = image.shape  # Directly using numpy shape property.
         # print("This is the shape being used", shape_n)
@@ -117,9 +117,9 @@ class RandomDoubleCrop1(object):
 
         # print("Cropped image 0 shape: ", padded_image.shape)
         nsample['random_crop_image_0'] = padded_image
-        padded_image_for_display = np.squeeze(padded_image)
-        cv2.imshow('Cropped Image', padded_image_for_display)
-        cv2.waitKey(1000)
+        # padded_image_for_display = np.squeeze(padded_image)
+        # cv2.imshow('Cropped Image', padded_image_for_display)
+        # cv2.waitKey(1000)
         nsample['random_position_0'] = random_pos0
 
         # # Optional: Draw the square and midpoint on a copy of the image for visualization
@@ -188,4 +188,3 @@ class ToPositionTensor1(object):
         # sample['rela_fullsize_distance'] = (sample['random_fullsize_position_0'] - sample['random_fullsize_position_1'])
 
         return sample
-

train/train_prnet.py
@@ -81,26 +81,24 @@ def train(config_file):
             # print(img_batch0)
             # print(img_batch0.shape)
             # Forward pass
-
-            predic_0 = prnet(img_batch0)
-            predic_1 = prnet(img_batch1)
+            image1 = img_batch0/255
+            image2 = img_batch1/255
+            predic_0 = prnet(image1)
+            predic_1 = prnet(image2)
             predic_ae_0, predic_cor_fc_0 = torch.sigmoid(predic_0['ae']), predic_0['fc_position']
             # print(predic_ae_0)
             # print(predic_ae_0.shape)
             predic_ae_1, predic_cor_fc_1 = torch.sigmoid(predic_1['ae']), predic_1['fc_position']
 
-            img_batch0_normalized = img_batch0 / 255.0
-            img_batch1_normalized = img_batch1 / 255.0
-
             vutils.save_image(predic_ae_0.data,
                               os.path.join(output_dir1, f'predic_ae_0_epoch_{epoch}_batch_{i_batch}.png'),
                               normalize=True)
 
-            vutils.save_image(img_batch0.data,
+            vutils.save_image(image1.data,
                               os.path.join(output_dir2, f'img_batch0_epoch_{epoch}_batch_{i_batch}.png'),
                               normalize=False)
 
-            ae_train_loss = loss_func(predic_ae_0, img_batch0_normalized) + loss_func(predic_ae_1, img_batch1_normalized)
+            ae_train_loss = loss_func(predic_ae_0, img_batch0) + loss_func(predic_ae_1, img_batch1)
             fc_predic = dis_ratio * torch.tanh(predic_cor_fc_0 - predic_cor_fc_1)
             fc_train_loss = loss_func(fc_predic, rela_distance_batch)
             train_loss = ae_train_loss + fc_train_loss